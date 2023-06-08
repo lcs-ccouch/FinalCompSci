d62964c438f274a3bf830d7333c07f2e54eb3b5d
Calum Couch <ccouch@lcs.on.ca>
2023-06-07 22:05:57 -0400

    Initial Commit

---
+++ FinalCompSci/FinalCompSciApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalCompSciApp.swift
+//  FinalCompSci
+//
+//  Created by calum couch on 2023-06-07.
+//
+
+import SwiftUI
+
+@main
+struct FinalCompSciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
